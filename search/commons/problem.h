dff10f3316957dae81f46fda2e59cd36483601b5
jfong <jfong361@gmail.com>
2018-05-19 16:37:05 +1000

    Add problem definition

---
+++ search/commons/problem.h
@@ -0,0 +1,38 @@
+//
+// @author: jiahong
+// @date  : 19/05/18 4:26 PM
+//
+
+#ifndef AIMA_PROBLEM_H
+#define AIMA_PROBLEM_H
+
+#include<vector>
+
+template<typename S, typename A>
+class Problem {
+public:
+    /// given a state, return all possible actions that the provided state can take
+    /// \param state
+    ///         State where possible actions should be returned by this function
+    /// \return std::vector<A>
+    ///         A vector of actions that can be taken from the provided "start" state
+    virtual std::vector<A> actions(const S &state) const = 0;
+
+    /// given a state and action, apply the action on the state and return the successor node
+    /// \param state
+    ///         State where the action should be applied to
+    /// \param action
+    ///         Action that should be applied to state
+    /// \return state
+    ///         A brand new successor state (state is not modified) as a result of applying action to state
+    virtual S successor(const S &state, const A &action) const = 0;
+
+    /// given a state, checks if it is (one of / the) goal state
+    /// \param state
+    ///         State that should be checked for a goal state
+    /// \return bool
+    ///         Indication of whether or not state is a goal state
+    virtual bool goal(const S &state) const = 0;
+};
+
+#endif //AIMA_PROBLEM_H
