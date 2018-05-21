835bcc2c8d72179839964e9f0144e415a5769ac3
jfong <jfong361@gmail.com>
2018-05-22 01:43:38 +1000

    Add test cases for BFS

---
+++ test/search/commons/test-bfs.cpp
@@ -0,0 +1,55 @@
+//
+// @author: jiahong
+// @date  : 22/05/18 1:38 AM
+//
+
+
+#include "romania.h"
+#include "uninformed_search/bfs.h"
+#include "catch.hpp"
+
+
+TEST_CASE("Test breadth first search 1", "[TBFS1]") {
+    SECTION("BFS works on the Romanian map, from Arad to Pitesti") {
+        using namespace aima;
+        RomaniaMap problem("Arad", "Pitesti");
+        std::vector<std::string> solution = {"Arad", "Timisoara", "Lugoj", "Mehadia", "Drobeta", "Craiova", "Pitesti"};
+        auto res = bfs(problem);
+        REQUIRE(bool(res));
+        REQUIRE(res->path().front() == solution.front());
+        REQUIRE(res->path().back() == solution.back());
+    }
+
+    SECTION("BFS search works on the Romanian map, from Zerind to Eforie (very long path)") {
+        using namespace aima;
+        std::string start = "Zerind";
+        std::string end = "Eforie";
+        RomaniaMap problem(start, end);
+        auto res = bfs(problem);
+        REQUIRE(bool(res));
+        REQUIRE(res->path().front() == start);
+        REQUIRE(res->path().back() == end);
+    }
+
+    SECTION("BFS search works on the Romanian map, from Zerind to Neami(very long path)") {
+        using namespace aima;
+        std::string start = "Zerind";
+        std::string end = "Neami";
+        RomaniaMap problem(start, end);
+        auto res = bfs(problem);
+        REQUIRE(bool(res));
+        REQUIRE(res->path().front() == start);
+        REQUIRE(res->path().back() == end);
+    }
+
+    SECTION("BFS search works on the Romanian map, on a single node, goal node") {
+        using namespace aima;
+        std::string start = "Zerind";
+        std::string end = "Zerind";
+        RomaniaMap problem(start, end);
+        auto res = bfs(problem);
+        REQUIRE(bool(res));
+        REQUIRE(res->path().front() == start);
+        REQUIRE(res->path().back() == end);
+    }
+}
